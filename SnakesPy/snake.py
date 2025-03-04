3d2a68c2fa38b9bdd3c8237d66d62addbcb38cf3
Kenspectacle <Kennyftw2@gmail.com>
2025-03-04 23:58:33 +0100

    feat: created a snake class in python

---
+++ SnakesPy/snake.py
@@ -0,0 +1,8 @@
+class Snake:
+    def __init__(self, state, head, segments, current_direction):
+        self.state = state
+        self.head = head
+        self.segments = segments
+        self.current_direction  = current_direction
+    
+    
